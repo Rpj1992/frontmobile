14df60f7a116d446237c06c8dd61484be4c40386
Ruben Pulido <email@email.com>
2026-06-11 12:22:13 +0200

    ej3 funcional

--- src/main/java/es/acing/dim/frontmobile/TareaController.java
+++ src/main/java/es/acing/dim/frontmobile/TareaController.java
@@ -1,10 +1,12 @@
 package es.acing.dim.frontmobile;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TareaController {
@@ -20,4 +22,35 @@ public class TareaController {
         List<Tarea> listaTareas = tareaService.getListaTareas();
         return listaTareas;
     }
+
+    @PostMapping("/tareas")
+    public ResponseEntity<Tarea> postTarea(@RequestBody Tarea tarea) {
+        Tarea tareaCreada = (Tarea) tareaService.crearTarea(tarea);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tareaCreada);
+    }
+
+    @PutMapping("/tareas/{id}")
+    public ResponseEntity<Optional<Tarea>> putTarea(@PathVariable long id, @RequestBody Tarea tarea) {
+        Optional<Tarea> tareaActualizada = tareaService.actualizarTarea(id, tarea);
+
+        if (tareaActualizada.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(tareaActualizada);
+    }
+
+    @DeleteMapping("/tareas/{id}")
+    public ResponseEntity<Void> deleteTarea(@PathVariable long id) {
+
+        boolean eliminida = tareaService.eliminarTarea(id);
+
+        if (!eliminida) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/es/acing/dim/frontmobile/TareaService.java
+++ src/main/java/es/acing/dim/frontmobile/TareaService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TareaService {
@@ -24,4 +25,34 @@ public class TareaService {
     public void setListaTareas(List<Tarea> listaTareas) {
         this.listaTareas = listaTareas;
     }
+
+    public Tarea crearTarea(Tarea tarea) {
+        Tarea tareaCreada = tarea;
+        tareaCreada.setId(listaTareas.get(listaTareas.size()-1).getId()+1);
+        listaTareas.add(tareaCreada);
+        return tareaCreada;
+    }
+
+    public Optional<Tarea> actualizarTarea(long id, Tarea tarea) {
+        for (Tarea t : listaTareas) {
+            if (t.getId() == id) {
+                t.setCompletada(tarea.isCompletada());
+                t.setTitulo(tarea.getTitulo());
+                return Optional.of(t);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public boolean eliminarTarea(long id) {
+        for (int i = 0;i < listaTareas.size(); i++) {
+            if (listaTareas.get(i).getId() == id) {
+                listaTareas.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
